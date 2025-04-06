1ee2365226c911567bd0f1ca7a21efa12319b880
estrengermithila <mithilafarjanam20@gmail.com>
2025-04-06 16:58:10 +0600

    assignment 10 login form design

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className='bg-[url(./src/assets/funding.png)] w-full h-screen bg-no-repeat bg-cover bg-center absolute top-0 left-0 -z-10'>
+            <div className='bg-black/50 w-full min-h-screen'>
+                <div className='w-full min-h-screen flex items-center justify-center'>
+                    <div className='w-[400px] min-h-[250px] bg-white p-8'>
+                        <form className='w-full'>
+                            <div className='space-y-5'>
+                                <div className='w-full'>
+                                    <input type="email" placeholder='Email' className='w-full border border-slate-400 pl-4 py-2'/>
+                                </div>
+                                <div className='w-full'>
+                                    <input type="password" placeholder='Password' className='w-full border border-slate-400 pl-4 py-2'/>
+                                </div>
+                            </div>
+                            <div className='w-full mt-4 text-right'>
+                                <Link className='text-sm underline'>Forget Password</Link>
+                            </div>
+                            <div className='w-full mt-5'>
+                                <button className='w-full bg-blue-600 text-white font-semibold py-2'>Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
